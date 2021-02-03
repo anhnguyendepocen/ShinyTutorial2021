3d0318d84190fc2c846df159e9125d61087620ac
Michael (Mike) Kleinsasser <39565600+mkleinsa@users.noreply.github.com>
2021-02-03 09:14:08 -0500

    updated from workshop

--- app_template.R
+++ app_template.R
@@ -2,50 +2,77 @@
 library(shiny)
 library(tidyverse)
 
-options(shiny.launch.browser = .rs.invokeShinyWindowViewer)
+options(shiny.launch.browser = .rs.invokeShinyPaneViewer)
 
-# User interface source
+write.csv(select(starwars, name:species), "starwars.csv")
 
-# Fluid page -
-#  - Sidebar layout
-#    - sidebar panel
-#      - Upload data
-#      - Select verb
-#      - Add verb
-#      - Update output
-#    - Main panel
-#      - Output preview
-ui = fluidPage()
-
-
-# Server source
+ui = fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("upload_data", "Upload your data set"),
+      radioButtons("verb_type", "Choose a new verb for the chain", 
+                   choices = c("filter", "select", "mutate", 
+                               "arrange", "summarise")),
+      actionButton("action_add_verb", "Add selected verb"),
+      actionButton("update", "Update data"),
+      div(id = "option_space")
+    ),
+    mainPanel(
+      tableOutput("preview")
+    )
+  )
+)
 
 server = function(input, output) {
-  # data - store read data and verb ID
-  # set current data, temp. data, and ID to NULL
+  data = reactiveValues()
+  current = NULL
+  temp = NULL
+  data$id = NULL
   
-  # Observe file input button
-  #  - read and store data from file input object
+  observeEvent(input$upload_data, {
+    data$raw = read.csv(file = input$upload_data$datapath)
+  })
   
-  # Observe action button add verb
-  #  - check current data for validity
-  #  - insert UI element for very
-  #    - switch between selected verb
-  #      for each verb type, update ID and return 
-  #      verb UI element
+  observeEvent(input$action_add_verb, {
+    if(is.null(current)) { current <<- data$raw }
+    insertUI(
+      selector = "#option_space",
+      ui = switch(
+        input$verb_type,
+        filter = {div()},
+        select = {data$id <- "select_ckbxgroup"; div(
+          checkboxGroupInput("select_ckbxgroup", "Select columns for select", 
+                             choices = names(current))
+        )},
+        mutate = {div()},
+        arrange = {data$id <- "select_rad_arrange"; div(
+          checkboxGroupInput("select_rad_arrange", "Select columns for arrange",
+                             choices = names(current))
+        )},
+        summarise = {div()}
+      )
+    )
+  })
   
-  # Observe update data
-  #  - set current data to temp data
-  #  - remove the previous UI element (clean up)
+  observeEvent(input$update, {
+    current <<- temp
+    removeUI(paste0('#',data$id))
+  })
   
-  # Preview verb applied to the data
-  #  - If data ID is not NULL
-  #    - if/else if to select the correct verb ID
-  #      - Update temp data by applying the verb
-  #      - return temp
+  output$preview = renderTable({
+    if(!is.null(data$id)) {
+      if(data$id == "select_ckbxgroup"){
+        req(input$select_ckbxgroup)
+        temp <<- current %>% select(!!input$select_ckbxgroup)
+        return(temp)
+      } else if(data$id == "select_rad_arrange") {
+        req(input$select_rad_arrange)
+        temp <<- current %>% arrange(desc(!!sym(input$select_rad_arrange)))
+        return(temp)
+      }
+    }
+  })
+  
 }
 
-
-# Launch app
 shinyApp(ui, server)
-
